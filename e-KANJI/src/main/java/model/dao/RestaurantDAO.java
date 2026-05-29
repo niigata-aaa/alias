254e8c5c27ba27b7a54dd1ec4c29b1d957f69293
emtech-user <emtech-user@PC-number>
2026-05-29 09:32:51 +0900

    restBean 松尾

--- e-KANJI/src/main/java/model/dao/RestaurantDAO.java
+++ e-KANJI/src/main/java/model/dao/RestaurantDAO.java
@@ -1,5 +1,33 @@
-package model.dao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class RestaurantDAO {
+import model.entity.RestBean;
 
-}
+public List<RestBean> selectAll() throws ClassNotFoundException, SQLException {
+		List<RestBean> list = new ArrayList<RestBean>();
+
+		String sql = "SELECT * FROM m_restaurant";
+
+		// データベースへの接続の取得、Statementの取得、SQLステートメントの実行
+		try(Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql)){
+			
+			
+			ResultSet res = pstmt.executeQuery();
+
+			// 結果の操作
+			while(res.next()) {
+				RestBean rest = new RestBean();
+				rest.setUserId(res.getString("user_id"));
+				rest.setName(res.getString("name"));
+				rest.setAge(res.getInt("age"));
+				list.add(rest);
+			}
+		}
+
+		return list;
+	}
--- e-KANJI/src/main/java/model/entity/RestBean.java
+++ e-KANJI/src/main/java/model/entity/RestBean.java
@@ -3,5 +3,123 @@ package model.entity;
 import java.io.Serializable;
 
 public class RestBean implements Serializable {
-
+	private int restId;
+	private String restName;
+	private String genre;
+	private String category;
+	private String open;
+	private String close;
+	private int nextday;
+	private int distance;
+	private int restBudget;
+	private int capacity;
+	private String tel;
+	private String address;
+	private String url;
+	private String restBeer;
+	private String restSmoke;
+	private String restSmokeroom;
+	
+	public RestBean() {
+		
+	}
+	
+	
+	
+	public int getRestId() {
+		return restId;
+	}
+	public void setRestId(int restId) {
+		this.restId = restId;
+	}
+	public String getRestName() {
+		return restName;
+	}
+	public void setRestName(String restName) {
+		this.restName = restName;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getOpen() {
+		return open;
+	}
+	public void setOpen(String open) {
+		this.open = open;
+	}
+	public String getClose() {
+		return close;
+	}
+	public void setClose(String close) {
+		this.close = close;
+	}
+	public int getNextday() {
+		return nextday;
+	}
+	public void setNextday(int nextday) {
+		this.nextday = nextday;
+	}
+	public int getDistance() {
+		return distance;
+	}
+	public void setDistance(int distance) {
+		this.distance = distance;
+	}
+	public int getRestBudget() {
+		return restBudget;
+	}
+	public void setRestBudget(int restBudget) {
+		this.restBudget = restBudget;
+	}
+	public int getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getRestBeer() {
+		return restBeer;
+	}
+	public void setRestBeer(String restBeer) {
+		this.restBeer = restBeer;
+	}
+	public String getRestSmoke() {
+		return restSmoke;
+	}
+	public void setRestSmoke(String restSmoke) {
+		this.restSmoke = restSmoke;
+	}
+	public String getRestSmokeroom() {
+		return restSmokeroom;
+	}
+	public void setRestSmokeroom(String restSmokeroom) {
+		this.restSmokeroom = restSmokeroom;
+	}
 }
