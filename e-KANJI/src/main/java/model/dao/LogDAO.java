88979e87b18f5cee17aa1b4180ddc7a30b36f402
emtech-user <emtech-user@PC-number>
2026-06-03 09:56:47 +0900

    dao

--- e-KANJI/src/main/java/model/dao/LogDAO.java
+++ e-KANJI/src/main/java/model/dao/LogDAO.java
@@ -24,4 +24,24 @@ public class LogDAO {
 		return count;
 
 	}
+	
+	
+	public int delete(String logUser) throws SQLException, ClassNotFoundException {
+		int count = 0; //処理件数
+		String sql = "DELETE FROM t_log WHERE log_user = ?";
+
+		// データベースへの接続の取得、PreparedStatementの取得
+		try (Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+			// プレースホルダへの値の設定
+			pstmt.setString(1, logUser);
+
+			// SQLステートメントの実行
+			count = pstmt.executeUpdate();
+		}
+
+		return count;
+
+	}
 }
